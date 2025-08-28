54ce7a9c37bd947beed38e0fb94cee34cef6b863
L0campo <226594654+L0campo@users.noreply.github.com>
2025-08-28 18:55:35 -0500

    secont commit

--- triangulos.py
+++ triangulos.py
@@ -1,26 +1,21 @@
 import os
 
-isActive=True
+isActive = True
 while isActive:
-
- os.system("clear")
-
- print("bienvenido al generador de triangulo")
- try:
-
-   n=int(input("ingrese la base del triangulo"))
-   if n==0:
-     isActive=0
-     print("gracias por usar el progrma")
-
-     for i in range(1, n+1):
-       print("*"* i)
-   elif n>1:
-     for i in range(1,n,+1):
-      print("*"*i)
-      input("presiona ENTER para continuar...")
-   
-   else:
-      print("no existe ningun triamgulo con base 1")
- except ValueError:
-   print("debe ingresar un numero entreo")
+    os.system("clear")
+    print("Bienvenido al generador de triangulos")
+    try:
+        n = int(input("Ingrese la cantidad de la base del triangulo\nPresione 0 si desea salir: "))
+        if n == 0:
+            isActive = False
+            print("Gracias por usar el programa")
+        elif n > 1:
+            for i in range(1, n+1):
+                print("*"*i)
+            input("Presione ENTER para continuar...")
+        else:
+            print("No existe un triangulo con base 1")
+            input("Presione ENTER para continuar...")
+    except ValueError:
+        print("Debe ingresar un numero entero")
+        input("Presione ENTER para continuar...")
